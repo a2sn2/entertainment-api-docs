c278350db51c399bfc6842491aa9887771376a6f
ALHassan ALShami <hassan1alshami6@gmail.com>
2026-08-04 17:21:25 +0300

    Harden EF identity foundation and secret hygiene

--- platform/src/EntertainmentDocs.Domain/Common/Entity.cs
+++ platform/src/EntertainmentDocs.Domain/Common/Entity.cs
@@ -4,5 +4,5 @@ public abstract class Entity
 {
     protected Entity(Guid id) => Id = id;
     protected Entity() { }
-    public Guid Id { get; protected init; }
+    public Guid Id { get; protected set; }
 }
