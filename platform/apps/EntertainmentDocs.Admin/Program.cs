f4eb90d55a30f09b1836e419739b21f8517e9b3e
ALHassan ALShami <hassan1alshami6@gmail.com>
2026-08-05 16:11:16 +0300

    Configure MudBlazor, authentication, and typed API clients

--- platform/apps/EntertainmentDocs.Admin/Program.cs
+++ platform/apps/EntertainmentDocs.Admin/Program.cs
@@ -1,6 +1,13 @@
 using EntertainmentDocs.Admin;
+using EntertainmentDocs.Admin.Features.Authentication;
+using EntertainmentDocs.Admin.Features.Documents;
+using EntertainmentDocs.Admin.Features.Users;
+using EntertainmentDocs.Admin.Infrastructure.Api;
+using EntertainmentDocs.Admin.Infrastructure.Authentication;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -15,4 +22,17 @@ var apiBase = string.IsNullOrWhiteSpace(configuredApiBase)
         : new Uri(applicationBase, configuredApiBase);
 
 builder.Services.AddScoped(_ => new HttpClient { BaseAddress = apiBase });
+builder.Services.AddMudServices();
+builder.Services.AddAuthorizationCore();
+
+builder.Services.AddScoped<IAccessTokenStore, BrowserAccessTokenStore>();
+builder.Services.AddScoped<JwtAuthenticationStateProvider>();
+builder.Services.AddScoped<AuthenticationStateProvider>(services =>
+    services.GetRequiredService<JwtAuthenticationStateProvider>());
+builder.Services.AddScoped<AuthenticatedRequestFactory>();
+
+builder.Services.AddScoped<AuthenticationApiClient>();
+builder.Services.AddScoped<UsersApiClient>();
+builder.Services.AddScoped<DocumentsApiClient>();
+
 await builder.Build().RunAsync();
