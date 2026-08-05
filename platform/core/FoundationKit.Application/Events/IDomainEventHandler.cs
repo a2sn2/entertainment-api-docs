a587f295ecf5e54a4ca1a832cf84aed6e2ce35ff
ALHassan ALShami <hassan1alshami6@gmail.com>
2026-08-05 20:54:27 +0300

    Add domain event application contracts

---
+++ platform/core/FoundationKit.Application/Events/IDomainEventHandler.cs
@@ -0,0 +1,13 @@
+using FoundationKit.Domain.Events;
+
+namespace FoundationKit.Application.Events;
+
+public interface IDomainEventHandler<in TEvent> where TEvent : IDomainEvent
+{
+    Task HandleAsync(TEvent domainEvent, CancellationToken cancellationToken = default);
+}
+
+public interface IDomainEventDispatcher
+{
+    Task DispatchAsync(IEnumerable<IDomainEvent> domainEvents, CancellationToken cancellationToken = default);
+}
