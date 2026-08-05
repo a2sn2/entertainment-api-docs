d61c7e7ea2c5a0fec275f07306029aad0ca0ff1f
ALHassan ALShami <hassan1alshami6@gmail.com>
2026-08-05 21:39:09 +0300

    Make transient entity equality safe

--- platform/core/FoundationKit.Domain/Primitives/Entity.cs
+++ platform/core/FoundationKit.Domain/Primitives/Entity.cs
@@ -1,3 +1,5 @@
+using System.Runtime.CompilerServices;
+
 namespace FoundationKit.Domain.Primitives;
 
 public abstract class Entity<TId> where TId : notnull
@@ -7,14 +9,21 @@ public abstract class Entity<TId> where TId : notnull
 
     public TId Id { get; protected set; }
 
+    private bool IsTransient => EqualityComparer<TId>.Default.Equals(Id, default!);
+
     public override bool Equals(object? obj)
     {
         if (obj is null || obj.GetType() != GetType()) return false;
         if (ReferenceEquals(this, obj)) return true;
-        return EqualityComparer<TId>.Default.Equals(Id, ((Entity<TId>)obj).Id);
+
+        var other = (Entity<TId>)obj;
+        if (IsTransient || other.IsTransient) return false;
+        return EqualityComparer<TId>.Default.Equals(Id, other.Id);
     }
 
-    public override int GetHashCode() => HashCode.Combine(GetType(), Id);
+    public override int GetHashCode() => IsTransient
+        ? RuntimeHelpers.GetHashCode(this)
+        : HashCode.Combine(GetType(), Id);
 
     public static bool operator ==(Entity<TId>? left, Entity<TId>? right) => Equals(left, right);
     public static bool operator !=(Entity<TId>? left, Entity<TId>? right) => !Equals(left, right);
