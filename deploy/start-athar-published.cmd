578ff4491e3316148aaa02b3216eb7ac94d1bde7
ALHassan ALShami <hassan1alshami6@gmail.com>
2026-08-07 00:56:39 +0300

    Keep published admin password out of command arguments

--- deploy/start-athar-published.cmd
+++ deploy/start-athar-published.cmd
@@ -1,6 +1,5 @@
 @echo off
 setlocal
 cd /d "%~dp0"
-set /p ATHAR_ADMIN_PASSWORD=Enter a temporary admin password: 
-powershell -NoProfile -ExecutionPolicy Bypass -File ".\START-PUBLISHED-ATHAR.ps1" -AdminPassword "%ATHAR_ADMIN_PASSWORD%"
+powershell -NoProfile -ExecutionPolicy Bypass -File ".\START-PUBLISHED-ATHAR.ps1"
 if errorlevel 1 pause
