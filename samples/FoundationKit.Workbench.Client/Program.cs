431b83af3a4161776bb02508e8e2225b10691f4f
ALHassan ALShami <hassan1alshami6@gmail.com>
2026-08-06 15:58:58 +0300

    Configure Blazor WebAssembly and MudBlazor

---
+++ samples/FoundationKit.Workbench.Client/Program.cs
@@ -0,0 +1,18 @@
+using FoundationKit.Workbench.Client;
+using FoundationKit.Workbench.Client.Services;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(_ => new HttpClient
+{
+    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+});
+builder.Services.AddScoped<WorkbenchApiClient>();
+builder.Services.AddMudServices();
+
+await builder.Build().RunAsync();
