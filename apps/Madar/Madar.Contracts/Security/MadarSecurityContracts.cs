29c7bdf24ef1534db650e3b6f40d1af53032ddc8
ALHassan ALShami <hassan1alshami6@gmail.com>
2026-08-08 12:08:12 +0300

    Define Madar product roles

---
+++ apps/Madar/Madar.Contracts/Security/MadarSecurityContracts.cs
@@ -0,0 +1,9 @@
+namespace Madar.Contracts.Security;
+
+public static class MadarRoles
+{
+    public const string Requester = "Requester";
+    public const string Operator = "Operator";
+    public const string Supervisor = "Supervisor";
+    public const string Administrator = "Administrator";
+}
