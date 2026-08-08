7cf1050e3c4a12fd3ff1f61090b4e81fcdd0b1e0
ALHassan ALShami <hassan1alshami6@gmail.com>
2026-08-08 12:33:50 +0300

    Wire Madar client HTTP and authentication services

--- apps/Madar/Madar.Client/Program.cs
+++ apps/Madar/Madar.Client/Program.cs
@@ -1,4 +1,6 @@
 using Madar.Client;
+using Madar.Client.Services;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
@@ -6,6 +8,16 @@ using MudBlazor.Services;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(_ => new HttpClient
+{
+    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+});
 builder.Services.AddMudServices();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<MadarApiClient>();
+builder.Services.AddScoped<MadarAuthenticationStateProvider>();
+builder.Services.AddScoped<AuthenticationStateProvider>(services =>
+    services.GetRequiredService<MadarAuthenticationStateProvider>());
 
 await builder.Build().RunAsync();
